c3d3c90d97d586c4276dbe07b0baadac979e2cca
L4ntaarn <sho-nagamine-k22h@stu.kbc.ac.jp>
2023-01-11 09:48:25 +0900

    さよなら複数追加

--- GitTest/test01/ソース.cpp
+++ GitTest/test01/ソース.cpp
@@ -5,6 +5,18 @@ int main(void)
 	printf("Hello Git World !!\n");
 
 	printf("‚±‚ñ‚É‚¿‚Í");
+	printf("‚³‚æ‚È‚ç");
+	printf("‚³‚æ‚È‚ç");
+	printf("‚³‚æ‚È‚ç");
+	printf("‚³‚æ‚È‚ç");
+	printf("‚³‚æ‚È‚ç");
+	printf("‚³‚æ‚È‚ç");
+	printf("‚³‚æ‚È‚ç");
+	printf("‚³‚æ‚È‚ç");
+	printf("‚³‚æ‚È‚ç");
+	printf("‚³‚æ‚È‚ç");
+
+
 
 	return 0;
 }
